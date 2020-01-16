988c6a68d03fab2f71e1d8446333ed3c472222e5
Pabilito <40524000+Pabilito@users.noreply.github.com>
2020-01-16 18:05:12 +0100

    Buttons

--- Assets/MyAssets/Scripts/MenuButtonHandler.cs
+++ Assets/MyAssets/Scripts/MenuButtonHandler.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuButtonHandler : MonoBehaviour
 {
     public CharacterControl cc;
     public Button menu;
     public GameObject MenuPanel;
+    public Button resume;
+    public Button returnToMenu;
+    public Button restart;
+    public string levelName;
+    
 
     void Start()
     {
         menu.GetComponent<Button>().onClick.AddListener(TaskOnMenu);
+        resume.GetComponent<Button>().onClick.AddListener(TaskOnResume);
+        returnToMenu.GetComponent<Button>().onClick.AddListener(TaskOnReturnToMenu);
+        restart.GetComponent<Button>().onClick.AddListener(TaskOnRestart);
+
         MenuPanel = GameObject.FindGameObjectWithTag("MenuPanel");
         MenuPanel.SetActive(false);
     }
@@ -22,7 +32,20 @@ public class MenuButtonHandler : MonoBehaviour
         MenuPanel.SetActive(true);
     }
 
+    void TaskOnResume()
+    {
+        cc.gameWon = false;
+        MenuPanel.SetActive(false);
+    }
 
+    void TaskOnReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 
+    void TaskOnRestart()
+    {
+        SceneManager.LoadScene(levelName);
+    }
 
 }
