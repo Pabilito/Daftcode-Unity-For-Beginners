db26c5b58a7444268c77e7f211bdbb337c89f433
Pabilito <40524000+Pabilito@users.noreply.github.com>
2020-01-16 19:10:51 +0100

    tutorial

---
+++ Assets/MyAssets/Scripts/TutorialText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialText : MonoBehaviour
+{
+    public Button acknowledge;
+    public CharacterControl cc;
+
+    void Start()
+    {
+        acknowledge.GetComponent<Button>().onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        cc.gameWon = false;
+        acknowledge.enabled = false;
+    }
+}
